47c883a70fcc4f49d984738d545c935511b47be4
Stephen Mekosh <smekosh@users.noreply.github.com>
2018-08-28 18:56:46 -0400

    Updated app store links

--- translations.php
+++ translations.php
@@ -8,9 +8,9 @@ $translations = array(
 		'headline'  => 'اپلکیشن جدید ما را دانلود کنید',
 		'subhead'   => 'اپلکیشن در اپل ستور و پلی ستور در دسترس است',
 		'ios_alt'   => 'دانلود اپلیکشین صدای امریکا از اپل ستور',
-		'ios_url'   => '',
+		'ios_url'   => 'https://itunes.apple.com/app/id1410784178',
 		'play_alt'  => 'دانلود اپلیکشین صدای امریکا از گوگل پلی',
-		'play_url'  => ''
+		'play_url'  => 'https://play.google.com/store/apps/details?id=com.voanews.voaafghan'
 	),
 	
 	'english' => array(
@@ -30,7 +30,7 @@ $translations = array(
 		'headline'  => 'Téléchargez la nouvelle application VOA aujourd’hui',
 		'subhead'   => 'Disponible sur iOS et Android',
 		'ios_alt'   => 'Téléchargez sur l’App Store',
-		'ios_url'   => 'https://itunes.apple.com/app/voa-afrique/id1390545562',
+		'ios_url'   => 'https://itunes.apple.com/app/id1390545562',
 		'play_alt'  => 'Téléchargez sur Google Play',
 		'play_url'  => ''
 	),
@@ -41,7 +41,7 @@ $translations = array(
 		'headline'  => 'Sauke sabuwar manhajar VOA a yau',
 		'subhead'   => 'Ana samu a wayoyin iOS da na Android',
 		'ios_alt'   => 'Za a iya sauke manhajar daga rumbun iOS',
-		'ios_url'   => 'https://itunes.apple.com/app/voa-hausa/id1387857853',
+		'ios_url'   => 'https://itunes.apple.com/app/id1387857853',
 		'play_alt'  => 'Za a iya samu a rumbun Google Play',
 		'play_url'  => 'https://play.google.com/store/apps/details?id=com.audionowdigital.player.voahausa'
 	),
@@ -52,7 +52,7 @@ $translations = array(
 		'headline'  => 'Telechaje app VOA Kreyòl la Jodi a!',
 		'subhead'   => 'Ou kapab telechaje l nan iPhone ak Android',
 		'ios_alt'   => 'Telechaje l nan App Store',
-		'ios_url'   => 'https://itunes.apple.com/app/voa-krey%C3%B2l/id1363120940',
+		'ios_url'   => 'https://itunes.apple.com/app/id1363120940',
 		'play_alt'  => 'W ap jwenn li nan Google Play',
 		'play_url'  => 'https://play.google.com/store/apps/details?id=com.audionowdigital.player.voakreyol'
 	),
@@ -63,9 +63,20 @@ $translations = array(
 		'headline'  => '下载美国之音新闻应用程序',
 		'subhead'   => '',
 		'ios_alt'   => '通过苹果商店下载',
-		'ios_url'   => 'https://itunes.apple.com/app/%E7%BE%8E%E5%9B%BD%E4%B9%8B%E9%9F%B3/id1334369412?mt=8',
+		'ios_url'   => 'https://itunes.apple.com/app/id1334369412',
 		'play_alt'  => '通过Google Play 商店下载',
 		'play_url'  => 'https://play.google.com/store/apps/details?id=com.voanews.voazh'
+	),
+	
+	'pashto' => array(
+		'lang'      => '',
+		'dir'       => 'rtl',
+		'headline'  => 'زموږ نوی اپلکیشن ډانلوډ کړئ',
+		'subhead'   => 'اپلکیشن په اپل سټور او پلې سټور کې موندلی شی',
+		'ios_alt'   => 'د اپل ستور نه د امریکا غږ د اپلکیشن ډانلوډول',
+		'ios_url'   => 'https://itunes.apple.com/app/id1410784178',
+		'play_alt'  => 'د ګوګل پلی نه د امریکا غږ د اپلکیشن ډانلوډول',
+		'play_url'  => 'https://play.google.com/store/apps/details?id=com.voanews.voaafghan'
 	),
 	
 	'urdu' => array(
@@ -76,9 +87,7 @@ $translations = array(
 		'ios_alt'   => 'ایپل اسٹور سے ڈاون لوڈ کیجئے',
 		'ios_url'   => 'https://itunes.apple.com/app/id1405181675',
 		'play_alt'  => 'آپ اسے گوگل پلے پر بھی حاصل کرسکتے ہیں',
-		'play_url'  => ''
+		'play_url'  => 'https://play.google.com/store/apps/details?id=com.voanews.voaur'
 	)
-	
-	// urdu 
 	
 );
